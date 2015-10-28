a293ea8c59d90f3ee288bb6d2867198e4fda5ea7
Tania Jacob <tanzjacob@gmail.com>
2015-10-28 22:19:19 +1300

    Add enrolments basic model

---
+++ app/controllers/enrolments_controller.rb
@@ -0,0 +1,22 @@
+class EnrolmentsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @enrolment  = Enrolment.new
+  end
+
+  def create
+  end
+
+  def update 
+  end
+
+  def destroy
+  end
+
+end
---
+++ app/models/enrolment.rb
@@ -0,0 +1,10 @@
+class Enrolment < ActiveRecord::Base
+
+
+
+
+
+
+
+
+end
