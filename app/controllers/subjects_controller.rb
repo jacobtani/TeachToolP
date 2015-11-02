0dcdcdd26207dd8f54df04e8d958f8fecbb6694e
Tania Jacob <tanzjacob@gmail.com>
2015-11-02 22:41:19 +1300

    Subjects - create and editing

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
  class SubjectsController < ApplicationController
-  before_action :set_subject, only: [:update, :show, :destroy]
-  before_action :admin_only, only: [:new, :create, :update, :destroy]
+  before_action :set_subject, only: [:edit, :update, :show, :destroy]
+  #before_action :admin_only, only: [:new, :create, :update, :destroy]
   
   def index
     @subjects = Subject.all
@@ -11,45 +11,49 @@
   end
 
   def new
-    @subject  = Subject.new
+    @subject = Subject.new
   end
 
   def create
     @subject = Subject.new subject_params
-    if @subject.save
-      redirect_to root_path
-    else
-      redirect_to root_path
+    respond_to do |format|
+      if @subject.save
+        flash[:success] = "Subject was created successfully."
+        format.js { redirect_turbo subjects_path}
+      else
+        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @subject}, status: 500 }
+      end
+    end
   end
 
-    def update
+  def edit
+  end
+
+  def update
     respond_to do |format|
       if @subject.update_attributes subject_params
         flash[:success] = "Subject was updated successfully."
-        redirect_to root_path
-        #format.js {}
+        format.js {redirect_turbo subjects_path}
       else
-        redirect_to root_path
-        #format.js { render partial: 'shared/ajax_form_errors', locals: {model: @site}, status: 500 }
+        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @site}, status: 500 }
       end
     end
   end
 
   def destroy
     @subject.destroy
-    redirect_to root_path
+    redirect_to subjects_path
   end
 
   private
 
     def subject_params
-      params.require(:subject).permit(:name, :grade)
+      params.require(:subject).permit(:subject_name, :fee)
     end
 
     def set_subject
       @subject = Subject.find params[:id] rescue nil
       return not_found! unless @subject
     end
-
 
 end
