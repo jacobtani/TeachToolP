583a5504035b100f788127fac4d6c1309f6806a2
Tania Jacob <tanzjacob@gmail.com>
2015-11-03 22:45:52 +1300

    Make pack actions functional

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,7 +1,7 @@
 class PacksController < ApplicationController
   before_action :authenticate_user!  
-  before_action :set_pack, only: [:update, :show, :destroy]
-  before_action :admin_only, only: [:new, :create, :update, :destroy]
+  before_action :set_pack, only: [:edit, :update, :show, :destroy]
+  #before_action :admin_only, only: [:new, :create, :update, :destroy]
   
   def index
     @packs = Pack.all
@@ -16,11 +16,14 @@ class PacksController < ApplicationController
   end
 
   def create
-    @pack = Pack.new enclosure_params
-    if @pack.save
-      redirect_to root_path
-    else
-      redirect_to root_path
+    @pack = Pack.new pack_params
+    respond_to do |format|
+      if @pack.save
+        flash[:success] = "Pack was created successfully."
+        format.js { redirect_turbo admin_path}
+      else
+        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @pack}, status: 500 }
+      end
     end
   end
 
@@ -31,11 +34,9 @@ class PacksController < ApplicationController
     respond_to do |format|
       if @pack.update_attributes pack_params
         flash[:success] = "Pack was updated successfully."
-        redirect_to root_path
-        #format.js {}
+        format.js {redirect_turbo admin_path}
       else
-        redirect_to root_path
-        #format.js { render partial: 'shared/ajax_form_errors', locals: {model: @site}, status: 500 }
+        format.js { render partial: 'shared/ajax_form_errors', locals: {model: @pack}, status: 500 }
       end
     end
   end
@@ -48,7 +49,7 @@ class PacksController < ApplicationController
   private
 
     def pack_params
-      params.require(:pack).permit(:name, :subject)
+      params.require(:pack).permit(:name, :description, :action_required, :subject_id)
     end
 
     def set_pack
