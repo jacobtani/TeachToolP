66797a26d42e9a51e9b801e3534bafc9833f5bc8
Tania Jacob <tanzjacob@gmail.com>
2015-11-05 20:56:26 +1300

    Add parent-child relationship to User, update db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028095907) do
+ActiveRecord::Schema.define(version: 20151105073324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20151028095907) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.integer  "parent_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
