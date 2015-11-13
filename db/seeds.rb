847da0a4d63db6e73215bee470ee7d1530c46da1
Tania Jacob <tanzjacob@gmail.com>
2015-11-13 22:50:58 +1300

    Clean out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Subject.create(subject_name: 'Mathematics', fee: '98.00', lowest_grade_taught: '1', highest_grade_taught: '7')
 Subject.create(subject_name: 'English', fee: '98.00', lowest_grade_taught: '1', highest_grade_taught: '7')
 Pack.create(name: '5++', description: 'A pack for advanced fifth graders', action_required: 'Nothing', subject_id: 1)
