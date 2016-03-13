bd2395d487c6c5cccf42ba2d545350ff23b457e0
Tania Jacob <tanzjacob@gmail.com>
2016-03-13 16:44:09 +1300

    Add custom error page handling

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module TeachTool
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
     config.time_zone = 'Eastern Time (US & Canada)'
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
