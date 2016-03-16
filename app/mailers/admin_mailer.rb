48d610a8b8e4259473816d7a9bfa8d95d433240a
Tania Jacob <tanzjacob@gmail.com>
2016-03-16 22:48:14 +1300

    Fix content based on client feedback

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,7 +5,7 @@ class AdminMailer < ApplicationMailer
     @user = user
     full_message = user.full_name + "(" + user.id.to_s + ") needs help with " + message.subject_name + ". In particular pack " + message.pack_name + " on page " + message.page_number + ", question number " + message.question_number + ". The following additional details have been given: " + message.content
     if @user.role == 'student'
-      mail(from: @user.email, subject: message.message_subject, body: full_message)
+      mail(from: @user.email, subject: 'STUDENT NEEDS HELP', body: full_message)
     end
   end
 
@@ -13,31 +13,31 @@ class AdminMailer < ApplicationMailer
     @user = user
     full_message = user.full_name + "(" + user.id.to_s + ") needs help with " + message.subject_name + ". In particular pack " + message.pack_name + " on page " + message.page_number + ", question number " + message.question_number + ". The following additional details have been given: " + message.content
     if @user.role == 'parent'
-      mail(from: @user.email, subject: message.message_subject, body: full_message)
+      mail(from: @user.email, subject: 'GENERAL CORRESPONDENCE', body: full_message)
     end
   end
 
   def registration_confirmation_to_admin(user)
     @user = user
-    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: "A new Registration")
+    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: "ADDING NEW USER")
   end
 
   def missing_pack(user, message)
     @user = user
     full_message = user.full_name + ' wants to report a pack missing: ' + message.pack_name + ' with the following details: ' + message.content
-    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: message.message_subject, body: full_message)
+    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: 'MISSING PACK/WORK', body: full_message)
   end
 
   def payment_related_enquiry(user, message)
     @user = user
     full_message = @user.full_name + ' has the following payment related enquiry: ' + message.content
-    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: message.message_subject, body: full_message)
+    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: 'PAYMENT RELATED', body: full_message)
   end
 
   def general_parent_enquiry(user, message)
     @user = user
     full_message = @user.full_name + ' has the following general question: ' + message.content
-    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: message.message_subject, body: full_message)
+    mail(from: @user.email, to: 'tjterminator.dev@gmail.com', subject: 'GENERAL CORRESPONDENCE', body: full_message)
   end
 
   def redeem_reward(user, amount)
