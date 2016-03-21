0c5b2c178174bc3a1687ddfde5ece28db88f41c2
Tania Jacob <tanzjacob@gmail.com>
2016-03-21 22:35:07 +1300

    Fix email messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+
   def new
     @message = Message.new
   end
@@ -9,16 +10,13 @@ class MessagesController < ApplicationController
       if @message.message_subject == 'STUDENT NEEDS HELP'
         AdminMailer.student_enquiry(current_user, @message).deliver_now
         redirect_to student_view_path, notice: "Message sent!"
-      elsif @message.message_subject == 'PARENT NEEDS HELP'
-        AdminMailer.parent_enquiry(current_user, @message).deliver_now
-        redirect_to parent_summary_path
       elsif @message.message_subject == 'MISSING PACK/WORK'
         AdminMailer.missing_pack(current_user, @message).deliver_now
         redirect_to parent_summary_path
-      elsif @message.message_subject == 'PARENT GENERAL QUESTION'
+      elsif @message.message_subject == 'GENERAL CORRESPONDENCE'
         AdminMailer.general_parent_enquiry(current_user, @message).deliver_now
         redirect_to parent_summary_path
-      elsif @message.message_subject == 'PAYMENT RELATED MATTERS'
+      elsif @message.message_subject == 'PAYMENT RELATED'
         AdminMailer.payment_related_enquiry(current_user, @message).deliver_now
         redirect_to parent_summary_path
       elsif @message.message_subject == 'TERMINATED ACCOUNT'
@@ -36,10 +34,41 @@ class MessagesController < ApplicationController
     end
   end
 
+  def student_help_required
+    @message = Message.new
+  end
+
+  def missing_pack
+    @message = Message.new
+  end
+
+  def payment_related_enquiry
+    @message = Message.new
+  end
+
+  def general_parent_enquiry
+    @message = Message.new
+  end
+
+  def recommend_us
+    @message = Message.new
+  end
+
+  def missing_payment
+    UserMailer.missing_payment(@user).deliver_now
+    redirect_to parent_summary_path
+  end
+
+  def redeem_reward
+    AdminMailer.redeem_reward(@user, @user.rewards).deliver_now
+    @user.update(rewards: 0, activation_date: Date.today)
+    redirect_to parent_summary_path
+  end
+
   private 
 
-  def message_params
-    params.require(:message).permit(:content, :pack_name, :message_recipient_name, :page_number, :question_number, :subject_name, :subject, :message_subject, :message_recipient)
-  end
+    def message_params
+      params.require(:message).permit(:content, :pack_name, :message_recipient_name, :page_number, :question_number, :subject_name, :subject, :message_subject, :message_recipient)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,31 +11,31 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_scope :user do
     patch '/users/:id/edit' => "myregistrations#update", as: :edit_rego
-    get '/users/:id/children' => "myregistrations#children", as: :children
-    get '/users/:id/suspend' => "myregistrations#suspend", as: :suspend_child
-    get '/users/:id/cancel' => "myregistrations#cancel_account", as: :cancel_child
-    get '/users/:id/end_trial' => "myregistrations#end_trial", as: :end_trial
-    get '/users/:id/redeem_reward' => "myregistrations#redeem_reward", as: :redeem_reward
-    get '/users/:id/enter_placement_pack' => "myregistrations#enter_placement_pack", as: :enter_placement_pack
-    get '/users/:id/nullify_rewards' => "myregistrations#nullify_rewards", as: :nullify_rewards
   end
+
+  get '/users/:id/children' => "users#children", as: :children
+  get '/users/:id/suspend' => "users#suspend", as: :suspend_child
+  get '/users/:id/cancel' => "users#cancel_account", as: :cancel_child
+  get '/users/:id/end_trial' => "users#end_trial", as: :end_trial
+  get '/users/:id/nullify_rewards' => "users#nullify_rewards", as: :nullify_rewards
+  get '/users/:id/enter_placement_pack' => "users#enter_placement_pack", as: :enter_placement_pack
+  post '/users_admin/create' => "users#create", as: :create_user
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
   match "/401", :to => "errors#not_found", :via => :all
 
   get '/messages/missing_pack' => "messages#missing_pack", as: :missing_pack
-  get '/users/:id/general_parent_enquiry' => "messages#general_parent_enquiry", as: :general_parent_enquiry
-  get '/users/:id/parent_help_required' => "messages#parent_help_required", as: :parent_help
-  get '/users/:id/payment_related_enquiry' => "messages#payment_related_enquiry", as: :payment_related_enquiry
-  get '/users/:id/missing_payment' => "messages#missing_payment", as: :missing_payment
-  get '/users/:id/recommend_us' => "messages#recommend_us", as: :recommend_us
+  get '/messages/general_parent_enquiry' => "messages#general_parent_enquiry", as: :general_parent_enquiry
+  get '/messages/parent_help_required' => "messages#parent_help_required", as: :parent_help
+  get '/messages/payment_related_enquiry' => "messages#payment_related_enquiry", as: :payment_related_enquiry
+  get '/messages/missing_payment' => "messages#missing_payment", as: :missing_payment
+  get '/messages/recommend_us' => "messages#recommend_us", as: :recommend_us
+  get '/messages/redeem_reward' => "messages#redeem_reward", as: :redeem_reward
 
   resources :users_admin, :controller => 'users' do 
     resources :enrolments
   end
-
-  post '/users_admin/create' => "users#create", as: :create_user
 
   resources :packs
   resources :enclosures
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
   resources :fees
   resources :messages
   resources :pack_records
+
   get '/work_missing_email' => "pack_records#work_missing_email", as: :work_missing
 
   controller :pages do
