f3d7c9486c3f057ea2885b0b05fee108d1cdd8c0
Tania Jacob <tanzjacob@gmail.com>
2016-03-30 08:49:46 +1300

    Configure admin portal change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,18 +33,95 @@ $(document).ready(function() {
     $('.student-pack-records').removeClass('hidden');
     $('.student-details').addClass('hidden');
     $('.student-enrolments').addClass('hidden');
+ });
 
- })
  $('.details-title').on("click", function() {
     $('.student-pack-records').addClass('hidden');
     $('.student-details').removeClass('hidden');
     $('.student-enrolments').addClass('hidden');
- })
+ });
+
  $('.enrolments-title').on("click", function() {
     $('.student-pack-records').addClass('hidden');
     $('.student-details').addClass('hidden');
     $('.student-enrolments').removeClass('hidden');
- })
+});
+
+ $(".subject-page").addClass('hidden');
+ $(".offer-page").addClass('hidden');
+ $(".pack-page").addClass('hidden');
+ $(".enclosure-page").addClass('hidden');
+ $(".fee-page").addClass('hidden');
+ $(".user-page").addClass('hidden');
+ $(".all-users").addClass('hidden');
+
+ $(".subject-a").on("click", function() {
+    $(".subject-page").removeClass('hidden');
+    $(".offer-page").addClass('hidden');
+    $(".pack-page").addClass('hidden');
+    $(".enclosure-page").addClass('hidden');
+    $(".fee-page").addClass('hidden');
+    $(".user-page").addClass('hidden');
+    $(".all-users").addClass('hidden');
+    $(".user-admin").removeClass('hidden');
+});
+
+ $(".offer-a").on("click", function() {
+    $(".subject-page").addClass('hidden');
+    $(".offer-page").removeClass('hidden');
+    $(".pack-page").addClass('hidden');
+    $(".enclosure-page").addClass('hidden');
+    $(".fee-page").addClass('hidden');
+    $(".user-page").addClass('hidden');
+    $(".all-users").addClass('hidden');
+    $(".user-admin").removeClass('hidden');
+ });
+
+  $(".pack-a").on("click", function() {
+    $(".subject-page").addClass('hidden');
+    $(".offer-page").addClass('hidden');
+    $(".pack-page").removeClass('hidden');
+    $(".enclosure-page").addClass('hidden');
+    $(".fee-page").addClass('hidden');
+    $(".user-admin-page").addClass('hidden');
+    $(".user-parent-page").addClass('hidden');
+    $(".user-student-page").addClass('hidden');
+    $(".all-users").addClass('hidden');
+    $(".user-admin").removeClass('hidden');
+ });
+
+  $(".enclosure-a").on("click", function() {
+    $(".subject-page").addClass('hidden');
+    $(".offer-page").addClass('hidden');
+    $(".pack-page").addClass('hidden');
+    $(".enclosure-page").removeClass('hidden');
+    $(".fee-page").addClass('hidden');
+    $(".user-page").addClass('hidden');
+    $(".all-users").addClass('hidden');
+    $(".user-admin").removeClass('hidden');
+ });
+
+ $(".fee-a").on("click", function() {
+    $(".subject-page").addClass('hidden');
+    $(".offer-page").addClass('hidden');
+    $(".pack-page").addClass('hidden');
+    $(".enclosure-page").addClass('hidden');
+    $(".fee-page").removeClass('hidden');
+    $(".user-page").addClass('hidden');
+    $(".all-users").addClass('hidden');
+    $(".user-admin").removeClass('hidden');
+ });
+
+ $(".user-admin").on("click", function() {
+    $(".subject-page").addClass('hidden');
+    $(".offer-page").addClass('hidden');
+    $(".pack-page").addClass('hidden');
+    $(".enclosure-page").addClass('hidden');
+    $(".fee-page").addClass('hidden');
+    $(".user-page").removeClass('hidden');
+    $(".user-admin").addClass('hidden');
+    $(".all-users").removeClass('hidden');
+ });
 
 });
 
